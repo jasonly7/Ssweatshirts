e82bdd6c6263ea4872c25c3eb420999290230bf7
Jason Ly <gamedude_7@hotmail.com>
2025-06-06 14:49:20 -0400

    small tablet

--- app/src/main/java/com/peaceandcotton/sweatshirts/MainActivity.java
+++ app/src/main/java/com/peaceandcotton/sweatshirts/MainActivity.java
@@ -201,8 +201,8 @@ public class MainActivity extends AppCompatActivity {
                 if (selectedRadioButton != null) {
                     String selectedOption = selectedRadioButton.getText().toString();
                     ViewGroup.LayoutParams imageParams = selectedImage.getLayoutParams();
-                    int widthInPx = ConvertDpToPixels (150);
-                    int heightInPx = ConvertDpToPixels (150);
+                    int widthInPx = ConvertDpToPixels (100);
+                    int heightInPx = ConvertDpToPixels (100);
                     imageParams.width = widthInPx;
                     imageParams.height = heightInPx;
                     selectedImage.setLayoutParams(imageParams);
